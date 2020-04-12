bacc31eb5d6f9bdf99d2340f9bb0fd28007a07b1
hanumasri837 <63524574+hanumasri837@users.noreply.github.com>
2020-04-11 20:53:19 -0400

    Update checkprimenumber.java

--- Day-1/checkprimenumber.java
+++ Day-1/checkprimenumber.java
@@ -1,1 +1,39 @@
 
+import java.util.Scanner;
+
+class checkprimenumber {
+public static void main(String []args){
+
+int temp;
+boolean isprime= true;
+Scanner scanner = new Scanner(System.in);
+
+System.out.println("Enter any number");
+
+int number=scanner.nextInt();
+
+for (int i=2;i<=number/2;i++)
+{
+
+temp=num%i;
+if(temp==0)
+
+{
+isprime=false;
+break;
+}
+}
+
+if(isprime)
+{
+System.out.println("It is a prime number");
+}
+else
+{
+ System.out.println("It is not a prime number");
+}
+
+}
+
+}
+ 
