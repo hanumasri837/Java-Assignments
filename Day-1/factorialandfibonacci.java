e3ee2c04ff7e8b22c3a4019d5944bc57bdbf1f52
hanumasri837 <63524574+hanumasri837@users.noreply.github.com>
2020-04-11 21:09:33 -0400

    Update and rename factorial.java to factorialandfibonacci.java

--- Day-1/factorialandfibonacci.java
+++ Day-1/factorialandfibonacci.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class factorial
+
+public static class factorialandfibonacci
 
 {
 
@@ -9,6 +10,12 @@ public class factorial
  		return 1;
  	}
  	return n* factorial(n-1);
+ }
+ public static int fibonacci(int n){
+ 	if(n<=1){
+ 		return n;
+ 	}
+ 	return fibonacci(n-1)+ fibonacci(n-2);
  }
  public static void main(String []args)
 
@@ -21,8 +28,9 @@ System.out.println("Enter a number to find its factorial");
 int number=scanner.nextInt();
 
  System.out.println("Factorial of "+number+"is"+factorial(number));
-
-
-}
+System.out.println("Fibonacci series for the given number"+ number+"is " + fibonacci(number));
 
 }
+
+}
+ 
