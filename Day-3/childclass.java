41f90e135109af2ce38d89c510de5698bb38fb61
hanumasri837 <63524574+hanumasri837@users.noreply.github.com>
2020-04-18 19:30:22 -0400

    Update childclass.java

--- Day-3/childclass.java
+++ Day-3/childclass.java
@@ -1,12 +1,30 @@
 #inheritance
-
 class parentclass{
-    String str="Good morning" ;
+    protected String str="Good morning";
+    protected int a=10;
+    int b=20;
+    private void nowishes()
+    {
+        System.out.println(" private parent method");
+    }
+    protected void wishes(){
+        System.out.println(" protected parent method");
+    }
+    
     public void wish(){
-        System.out.println("parent class wish method");
+        System.out.println(" parent class wish method");
     }
 }
 public class childclass extends parentclass{
+    // @Override
+    // void wishes(){
+    //     System.out.println("wishes child class method");
+    // }
+    // @Override 
+    //void nowishes(){
+    //    System.out.println("child class nowishes method");
+    //}
+    
     @Override
     public void wish(){
         System.out.println("child class wish method");
@@ -15,7 +33,11 @@ public class childclass extends parentclass{
    public static void main(String args[]){
        childclass c = new childclass();
        c.wish();
+       c.wishes();
        System.out.println(c.str);
+       int sum= c.b+c.a;
+       System.out.println("sum of integers" + sum);
+       //   System.out.println(c.a); // private members
     
    }
     
