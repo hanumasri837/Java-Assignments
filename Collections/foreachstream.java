235c3e62efcd16705c20b874efcab2b1ff2984ad
hanumasri837 <63524574+hanumasri837@users.noreply.github.com>
2020-05-02 15:31:22 -0400

    Create foreachstream.java

---
+++ Collections/foreachstream.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class foreachstream{
+public static void main(String []args){
+
+List<Integer> listele=new ArrayList<Integer>();  
+listele.add(10);
+listele.add(11);
+listele.add(5);
+listele.add(7);
+
+listele.stream().forEach(ele -> System.out.println(ele));
+}
+}
