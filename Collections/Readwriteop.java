7f7c1ef62836bf269301eed4b919b7b8fd606779
hanumasri837 <63524574+hanumasri837@users.noreply.github.com>
2020-05-02 21:12:55 -0400

    Update Readwriteop.java

--- Collections/Readwriteop.java
+++ Collections/Readwriteop.java
@@ -3,10 +3,10 @@
 public class Readwriteop{
     
 public void setImplementation(){
-    Set<Integer> setele=new HashSet<Integer>();
+    Set<Integer> setEle=new HashSet<Integer>();
     for(int i =0;i< 10;i++)
-    setele.add(i);
-    for(int i:setele)
+    setEle.add(i);
+    for(int i:setEle)
     System.out.println(i);
     
 }
@@ -19,11 +19,11 @@ for (int i=0;i<listEle.size();i++)
 System.out.println(listEle.get(i));
 }
 public void mapImplementation(){
-    Map<Integer, String> mapele = new HashMap<Integer, String>();
-    mapele.put(1,"HTML");
-    mapele.put(2,"PHP");
-    for( int i : mapele.keySet())
-    System.out.println(""+i+"->"+ mapele.get(i));
+    Map<Integer, String> mapEle = new HashMap<Integer, String>();
+    mapEle.put(1,"HTML");
+    mapEle.put(2,"PHP");
+    for( int i : mapEle.keySet())
+    System.out.println(""+i+"->"+ mapEle.get(i));
 }
 public static void main(String []args){
 Readwriteop op= new Readwriteop();
