d5e14f40d7b246691536a4d869cb4178e6cd1dc1
hanumasri837 <63524574+hanumasri837@users.noreply.github.com>
2020-05-11 16:45:59 -0400

    Update EqualsExample.java

--- Collections/EqualsExample.java
+++ Collections/EqualsExample.java
@@ -1,7 +1,3 @@
-//-- Override equals and hashcode methods in a class and use them collection framework related classes
-
-
-
 import java.util.*;
 
 class HashCodeexample{
@@ -43,11 +39,17 @@ public int hashCode(){
 public class EqualsExample{
     
     public static void main(String []args){
-        HashCodeexample hash= new HashCodeexample(1);
-        HashCodeexample hash1= new HashCodeexample(2);
-        HashCodeexample hash2= new HashCodeexample(1);
-    System.out.println(hash.equals(hash1));
-    System.out.println(hash.equals(hash2));
+        ArrayList<HashCodeexample> arr= new ArrayList<HashCodeexample>();
+        arr.add(new HashCodeexample(1));
+        arr.add(new HashCodeexample(2));
+        arr.add(new HashCodeexample(1));
+        if(arr.get(1).equals(arr.get(2))){
+            System.out.println("object at index1 is not equal to object at index 2");
+        }
+        if(arr.get(0).equals(arr.get(2))){
+             System.out.println("object at index 0 equal to object at index 2");
+        }
+
 
     }
 }
