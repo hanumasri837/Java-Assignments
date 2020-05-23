b53c0b42494324e80bba30a7c5ca1778f57edb6c
hanumasri837 <63524574+hanumasri837@users.noreply.github.com>
2020-05-22 20:47:52 -0400

    Update jdbcone.java

--- JDBC/jdbcone.java
+++ JDBC/jdbcone.java
@@ -1,1 +1,31 @@
+import java.sql.*;
+import java.util.*;
 
+class jdbcone{
+  public static void main (String a[]) throws SQLException{
+  try{
+    Driver d= new JdbcOdbcDriver(); //drivers are loaded
+    DriverManager.registerDriver(d); //drivers registered
+    Connection con=getConnection("jdbc:odbc:jdbcdsn","user","password");
+    Statement st=con.createStatement(); //statement object is created
+    Resultset rs= st.executeQuery("Select deptno from employee"); //retrieving all deptartment numbers from employee table
+    if(rs!=null)
+    {
+      int dno =rs.getInt(1);
+      System.out.println(dno);
+    }
+    else{
+      System.out.println("Records not found");
+    }
+    
+  }
+    catch Exception e{
+      e.printStackTrace();
+    }
+    finally{
+      st.close();
+      con.close();
+    }
+  
+  }
+}
