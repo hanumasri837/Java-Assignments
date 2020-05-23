d1a0bb969b79649efe3665c7916aaab7789299b6
hanumasri837 <63524574+hanumasri837@users.noreply.github.com>
2020-05-23 03:15:26 -0400

    Update jdbctwo.java

--- JDBC/jdbctwo.java
+++ JDBC/jdbctwo.java
@@ -1,1 +1,25 @@
+//Write program to upload a file(to DB) and download file(from DB) using JDBC.
+import java.util.*;
+import java.sql.*;
 
+Class jdbctwo{
+  
+    public static void main (String a[]) throws SQLException, ClassNotFoundException{
+  try{
+    Class.forName("com.mysql.jdbc.Driver");
+    Connection con=DriverManager("jdbc:mysql://localhost:1234/jdbc","user","password");
+    
+    
+  }
+    catch Exception e{
+      e.printStackTrace();
+    }
+    finally{
+      st.close();
+      con.close();
+      cst.close();
+    }
+  
+ 
+  }
+}
