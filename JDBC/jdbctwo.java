79b8f21f5a5bbb81f3cce84b309ca10ac1a99c3c
hanumasri837 <63524574+hanumasri837@users.noreply.github.com>
2020-05-29 17:17:58 -0400

    Update jdbctwo.java

--- JDBC/jdbctwo.java
+++ JDBC/jdbctwo.java
@@ -8,13 +8,35 @@ Class jdbctwo{
   try{
     Class.forName("com.mysql.jdbc.Driver");
     Connection con=DriverManager("jdbc:mysql://localhost:1234/jdbc","user","password");
-    
+    //uploading file
+    PreparedStatement p= con.prepareStatement("update employee set resume= ?"+"where name='john'");
+    File file= new File("C:\Users\hanumasri\OneDrive\Documents\file.txt");
+    FileInputStream newfile= new FileInputStream(file);
+    p.setBinaryStream(4,newfile);
+    p.executeUpdate();
+    // reading file
+        String str="select resume from employee where name='john'";
+    Statement s=con.createStatement();    
+    result= s.executeQuery(str);
+        File outputfile= new File("outputfile.txt");
+        FileOutputStream outfile= new FileOutputStream(outputfile);
+        if(result.next()){
+          input= result.getBinaryStream("resume");
+          byte[] buffer=new byte[1024];
+          while(input.read(buffer)>0)
+          {
+            outfile.write(buffer);
+          }
+        }
     
   }
     catch Exception e{
       e.printStackTrace();
     }
     finally{
+      
+        outfile.close();
+        input.close();
       st.close();
       con.close();
       cst.close();
