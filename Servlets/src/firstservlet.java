b6a712d7067ec32f494548d66e776d80caf3078c
hanumasri837 <63524574+hanumasri837@users.noreply.github.com>
2020-05-29 18:22:56 -0400

    Update firstservlet.java

--- Servlets/src/firstservlet.java
+++ Servlets/src/firstservlet.java
@@ -1,1 +1,14 @@
 
+import java.util.*;
+import java.io.*;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class FirstServlet extends HttpServlet 
+{
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException{
+	PrintWriter out= res.getWriter();
+	out.print("Welcome to servlet");
+	}
+}
